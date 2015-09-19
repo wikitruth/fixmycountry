b64b6ee6de71142502d89c09d40c435af89801ec
Daniel Salunga <dsalunga@live.com>
2015-09-19 10:56:23 +0800

    v0.1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var server;
  */
 
 server = http.createServer(app);
-server.listen(process.env.PORT || 8000);
+server.listen(process.env.PORT || 7000);
 server.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
 });
